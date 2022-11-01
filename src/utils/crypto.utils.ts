3a070bcf4a28d62b4218fa1a0f10573678a36d2d
Joseph Enebe <enebejoe95@yahoo.com>
2022-10-31 23:38:33 -0700

    Update

--- src/utils/crypto.utils.ts
+++ src/utils/crypto.utils.ts
@@ -4,3 +4,26 @@ import { constant } from "../configs/constant.config";
 import { config } from "dotenv";
 
 // Enebe Joseph Atima SCI17CSC068
+config();
+const { cryptoSecretKey } = constant;
+
+const simpleCrypto = new SimpleCrypto('9bb61363-7a59-4686-b9da-f9dbcdc6bea3');
+
+export const encryptToken = (token: string): string => {
+  const cipherText = simpleCrypto.encrypt(token);
+  return cipherText;
+};
+
+export const decryptToken = (auth: string): PlainData => {
+  if (!auth || auth === "")
+    throw new AuthenticationError("Not authentication!");
+
+  const bearer = auth.split(" ")[0];
+  const cipherText = auth.split(" ")[1];
+
+  if ((bearer !== "Bearer" && bearer !== "Token") || cipherText === "")
+    throw new AuthenticationError("Not authentication!");
+
+  const token = simpleCrypto.decrypt(cipherText);
+  return token;
+};
